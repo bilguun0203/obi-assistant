d2938f942cd2e6562405d6ecbeb250ffd3d7f526
Bilguun Ochirbat <bilguun0203@gmail.com>
2019-05-02 13:32:50 +0800

    NLU модель унших тусдаа функц

--- obi-assistant.py
+++ obi-assistant.py
@@ -4,6 +4,12 @@ from skills import skills
 import utils.tts as tts
 
 
+def load_model(path='nlu/model'):
+    print('Loading Model...')
+    nlu_model = nlu.load_model('nlu/model')
+    return model
+
+
 def request(str, obj):
     result = skills.dispatch(nlu_model.parse(s), obj)
     print(result['answer'])
@@ -12,8 +18,7 @@ def request(str, obj):
 
 
 if __name__ == "__main__":
-    print('Loading Model...')
-    nlu_model = nlu.load_model('nlu/model')
+    nlu_model = load_model('nlu/model')
     print('Assistant ready')
     tmp = None
     while True:

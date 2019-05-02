27a976efe19da33740a2483d9d600c2ebf0e9317
Bilguun Ochirbat <bilguun0203@gmail.com>
2019-05-02 13:59:44 +0800

    Smart speaker

---
+++ obi-speaker.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+from precise_runner import PreciseEngine, PreciseRunner
+import time
+from pixel_ring import pixel_ring
+import mraa
+import os
+
+
+# Pixel Ring initialization
+en = mraa.Gpio(12)
+if os.getuid() != 0:
+    time.sleep(1)
+en.dir(mraa.DIR_OUT)
+en.write(0)
+pixel_ring.set_brightness(20)
+
+
+def on_activation():
+    print('hello')
+    pixel_ring.wakeup()
+    time.sleep(3)
+    pixel_ring.off()
+
+
+def main():
+    # Wake word listener initialization
+    engine = PreciseEngine(
+        'dist/precise-engine/precise-engine', 'dist/hey-obi/hey-obi.pb')
+    runner = PreciseRunner(engine, on_activation=on_activation)
+
+    runner.start()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        pixel_ring.off()
+        en.write(1)

7e9bec95db66cf112ca32ef3a7c422f990adf71e
Bilguun Ochirbat <bilguun0203@gmail.com>
2019-05-23 21:09:32 +0800

    засвар

--- obi_speaker.py
+++ obi_speaker.py
@@ -37,13 +37,13 @@ def record(duration=6):
     stream = audio.open(format=FORMAT, channels=CHANNELS,
                         rate=RATE, input=True,
                         frames_per_buffer=CHUNK)
-    print "recording..."
+    print("recording...")
     frames = []
 
     for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
         data = stream.read(CHUNK)
         frames.append(data)
-    print "finished recording"
+    print("finished recording")
 
     stream.stop_stream()
     stream.close()
